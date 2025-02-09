480dff595a38b021e19d262cb23b703ce857446c
moise10r <moiserushanika2006@gmail.com>
2025-02-09 21:25:24 +0200

    Implement other tests for SuperheroController

--- src/superhero/superhero.controller.spec.ts
+++ src/superhero/superhero.controller.spec.ts
@@ -3,10 +3,17 @@ import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { SuperheroController } from './superhero.controller';
 import { SuperheroService } from './superhero.service';
+import { CreateSuperheroDto } from '../dto';
 
 describe('SuperheroController (e2e)', () => {
   let app: INestApplication;
+  let controller: SuperheroController;
   let service: SuperheroService;
+
+  beforeEach(() => {
+    service = new SuperheroService();
+    controller = new SuperheroController(service);
+  });
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,7 +23,6 @@ describe('SuperheroController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-    service = moduleFixture.get<SuperheroService>(SuperheroService);
   });
 
   it('/GET superheroes (should return empty array initially)', async () => {
@@ -24,6 +30,40 @@ describe('SuperheroController (e2e)', () => {
       .get('/superheroes')
       .expect(200)
       .expect([]);
+  });
+
+  it('should add a superhero successfully', () => {
+    const superhero: CreateSuperheroDto = {
+      name: 'Batman',
+      superpower: 'Stealth',
+      humilityScore: 7,
+    };
+    expect(controller.addSuperhero(superhero)).toEqual({
+      message: 'Superhero added successfully',
+    });
+  });
+
+  it('should return superheroes sorted by humility score', () => {
+    controller.addSuperhero({
+      name: 'A',
+      superpower: 'Power',
+      humilityScore: 5,
+    });
+    controller.addSuperhero({
+      name: 'B',
+      superpower: 'Strength',
+      humilityScore: 9,
+    });
+    controller.addSuperhero({
+      name: 'C',
+      superpower: 'Speed',
+      humilityScore: 7,
+    });
+    expect(controller.getSuperheroes()).toEqual([
+      { name: 'B', superpower: 'Strength', humilityScore: 9 },
+      { name: 'C', superpower: 'Speed', humilityScore: 7 },
+      { name: 'A', superpower: 'Power', humilityScore: 5 },
+    ]);
   });
 
   afterAll(async () => {
